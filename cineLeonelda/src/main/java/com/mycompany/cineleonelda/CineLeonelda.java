65f9acd45cc879b1196ed7a0853255d94ea40384
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-12-05 14:35:13 -0500

    carpeta creada

---
+++ cineLeonelda/src/main/java/com/mycompany/cineleonelda/CineLeonelda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cineleonelda;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class CineLeonelda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
